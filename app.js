0cb4f5f0b3382f45526ad61ded5ec8fce3fbc266
Timryan3001 <timryan3001@gmail.com>
2023-10-24 14:19:33 +0100

    adding buttons to html

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let viz;
+
 console.log("hello back to school")
 
 // #1 create a variable to store the vizContainer
@@ -19,11 +21,38 @@ const url = "https://public.tableau.com/views/EmbeddingWorkbookProfitsAcrossME-A
 // #4 Creating the function to display our viz
 
 function initViz() {
-    const viz = new tableau.Viz(vizContainer, url, options);
+    viz = new tableau.Viz(vizContainer, url, options);
 }
 
 // #5 actually run the function
 
 initViz();
 
-// #6
+// #6 create a constant for our pdf button 
+
+const exportPdfButton = document.getElementById("exportPdf");
+
+// #7 create a function which exports a pdf
+
+function exportPdfFunction() {
+    viz.showExportPDFDialog();
+}
+
+// #8 call the function when button is pressed
+
+exportPdfButton.addEventListener("click", exportPdfFunction);
+
+
+// #9 create a constant for our ppt button 
+
+const exportPptButton = document.getElementById("exportPpt");
+
+// #10 create a function which exports a pdf
+
+function exportPptFunction() {
+    viz.showExportPowerPointDialog();
+}
+
+// #11 call the function when button is pressed
+
+exportPptButton.addEventListener("click", exportPptFunction);
