f80abe2036c46d002502e81b1b975515d22e72c3
Timryan3001 <timryan3001@gmail.com>
2023-10-24 14:50:55 +0100

    adding filters

--- app.js
+++ app.js
@@ -56,3 +56,23 @@ function exportPptFunction() {
 // #11 call the function when button is pressed
 
 exportPptButton.addEventListener("click", exportPptFunction);
+
+// #12 Filtering!!1
+
+function getRangeValues() {
+    const minValue = document.getElementById("minValue").value;
+    const maxValue = document.getElementById("maxValue").value;
+    console.log(minValue, maxValue);
+    // need to get active sheet, but this could be a dashboard or a worksheet
+    const workbook = viz.getWorkbook();
+    const activeSheet = workbook.getActiveSheet();
+    const sheets = activeSheet.getWorksheets();
+    //inspect the sheets you need to filter
+    console.log(sheets);
+    // index of the sheet you want to filter
+    const sheetToFilter = sheets[0]
+    // do the actual filtering
+    sheetToFilter.applyRangeFilterAsync("SUM(Sales)", { min: minValue, max: maxValue }).then(alert("viz filtered"));
+}
+
+document.getElementById("filterButton").addEventListener("click", getRangeValues);
